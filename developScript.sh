6b9bf755d0b668bfec29e342214a231e0a45ddcb
hillromChanakkya <chanakya.mathi@hill-rom.com>
2019-05-14 18:05:55 -0400

    updated build script

--- developScript.sh
+++ developScript.sh
@@ -8,7 +8,7 @@
 cd CI-CDApp
 echo "build"
 echo "$PWD"
-xcodebuild clean build -project CI-CDApp.xcodeproj -scheme CI-CDApp CODE_SIGNING_REQUIRED=NO
+xcodebuild clean build -project CI-CDApp.xcodeproj -scheme CI-CDApp CODE_SIGNING_REQUIRED=NO -sdk iphonesimulator12.2
 #CODE_SIGNING_ALLOWED="NO"
 
 
