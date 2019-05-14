258b9c768f607179ab2e94c36786da77f1e14661
hillromChanakkya <chanakya.mathi@hill-rom.com>
2019-05-14 13:57:13 -0400

    updated shell script

--- myScript.sh
+++ myScript.sh
@@ -6,4 +6,13 @@
 #  Created by chanakya mathi on 4/23/19.
 #  Copyright © 2019 Hill-Rom. All rights reserved.
 
-sleep 100s
+echo "$GIT_COMMIT"
+echo "$GIT_BRANCH"
+echo $GIT_LOCAL_BRANCH
+echo $GIT_PREVIOUS_COMMIT
+echo $GIT_PREVIOUS_SUCCESSFUL_COMMIT
+echo $GIT_URL
+echo $GIT_URL_N
+echo $GIT_AUTHOR_NAME
+echo $GIT_COMMITTER_EMAIL
+
