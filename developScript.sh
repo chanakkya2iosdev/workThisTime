868e50276b0ff1a76b2090fe78ff4d78b34f74cc
hillromChanakkya <chanakya.mathi@hill-rom.com>
2019-05-14 17:44:28 -0400

    updated build script

--- developScript.sh
+++ developScript.sh
@@ -6,7 +6,7 @@
 #  Created by chanakya mathi on 4/23/19.
 #  Copyright © 2019 Hill-Rom. All rights reserved.
 cd CI-CDApp
-echo "building"
+echo "build"
 echo "$PWD"
 xcodebuild clean build -project CI-CDApp.xcodeproj -scheme CI-CDApp CODE_SIGNING_REQUIRED=NO
 -destination ios simulator
