e0e92770b3c267ccfd65ed8c7606e5dafa5411c0
hillromChanakkya <chanakya.mathi@hill-rom.com>
2019-05-15 12:01:50 -0400

    changed develop script

--- developScript.sh
+++ developScript.sh
@@ -8,6 +8,6 @@
 cd CI-CDApp
 echo "build"
 echo "$PWD"
-xcodebuild clean build test -project CI-CDApp.xcodeproj -scheme CI-CDApp -sdk iphonesimulator CODE_SIGNING_ALLOWED="NO"
+xcodebuild clean build test -project CI-CDApp.xcodeproj -scheme CI-CDApp -sdk iphonesimulator CODE_SIGNING_ALLOWED="NO" | xcpretty && exit ${PIPESTATUS[0]}
 
 
