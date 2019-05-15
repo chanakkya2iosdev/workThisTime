a0cb62b08c6966b31c5dbfb5d1690db9c54abce5
hillromChanakkya <chanakya.mathi@hill-rom.com>
2019-05-15 17:06:53 -0400

    updated build script

--- developScript.sh
+++ developScript.sh
@@ -7,6 +7,6 @@
 #  Copyright © 2019 Hill-Rom. All rights reserved.
 set -eo pipefail
 cd CI-CDApp
-echo "build"
+echo "building and testing"
 echo "$PWD"
 xcodebuild -project CI-CDApp.xcodeproj -scheme CI-CDApp -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 7,OS=latest" CODE_SIGNING_REQUIRED=NO COSECODE_SIGNING_ALLOWED="NO" clean build test| xcpretty
