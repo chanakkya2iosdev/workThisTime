eb5e26f8392eece54598e9d4f4909e742713631d
hillromChanakkya <chanakya.mathi@hill-rom.com>
2019-05-15 11:29:47 -0400

    changed develop script

---
+++ CI-CDApp/CI-CDApp/SomeTestClass.swift
@@ -0,0 +1,15 @@
+//
+//  SomeTestClass.swift
+//  CI-CDApp
+//
+//  Created by chanakya mathi on 5/15/19.
+//  Copyright © 2019 hill-rom. All rights reserved.
+//
+
+import Foundation
+
+class GetSum {
+    func add(_ rhs: Int,_ lhs: Int) -> Int {
+        return rhs + lhs
+    }
+}
---
+++ CI-CDApp/CI-CDAppUnitTests/Test1.swift
@@ -0,0 +1,35 @@
+//
+//  Test1.swift
+//  CI-CDAppTests
+//
+//  Created by chanakya mathi on 5/15/19.
+//  Copyright © 2019 hill-rom. All rights reserved.
+//
+
+import XCTest
+@testable import CI_CDApp
+
+class Test1: XCTestCase {
+
+    let getSum = GetSum()
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+       XCTAssertEqual(getSum.add(1, 2), 3) 
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
