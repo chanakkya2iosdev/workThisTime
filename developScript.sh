f3d35d45407b08c7d35fda1696a2badc724b2a33
hillromChanakkya <chanakya.mathi@hill-rom.com>
2019-05-15 13:28:01 -0400

    updated script

--- developScript.sh
+++ developScript.sh
@@ -8,6 +8,6 @@
 cd CI-CDApp
 echo "build"
 echo "$PWD"
-xcodebuild -project CI-CDApp.xcodeproj -scheme CI-CDApp -sdk iphonesimulator -destination generic/platform="iOS Simulator" CODE_SIGNING_ALLOWED="NO" clean test| xcpretty
+xcodebuild -project CI-CDApp.xcodeproj -scheme CI-CDApp -sdk iphonesimulator -destination generic/platform="iOS Simulator" CODE_SIGNING_ALLOWED="NO" clean build| xcpretty
 
 
