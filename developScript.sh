9010cb60c71c736034b271735d2b2c2351830a36
hillromChanakkya <chanakya.mathi@hill-rom.com>
2019-05-15 20:02:22 -0400

    added sleep

--- developScript.sh
+++ developScript.sh
@@ -9,5 +9,7 @@ set -eo pipefail
 cd CI-CDApp
 echo "building and testing"
 echo "$PWD"
-sleep 40s
-xcodebuild -project CI-CDApp.xcodeproj -scheme CI-CDApp -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 7,OS=latest" CODE_SIGNING_REQUIRED=NO COSECODE_SIGNING_ALLOWED="NO" clean build test| xcpretty
+#sleep 40s
+DERIVED_DATA_DIR=DerivedData
+rm -rf $DERIVED_DATA
+xcodebuild -project CI-CDApp.xcodeproj -scheme CI-CDApp -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 7,OS=latest" CODE_SIGNING_REQUIRED=NO COSECODE_SIGNING_ALLOWED="NO" -derivedDataPath $(DERIVED_DATA) clean build test| xcpretty
