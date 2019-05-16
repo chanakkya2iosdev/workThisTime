02510d1de006777c68886461558a226fbf42b80a
hillromChanakkya <chanakya.mathi@hill-rom.com>
2019-05-15 20:58:19 -0400

    updated script

--- developScript.sh
+++ developScript.sh
@@ -9,5 +9,6 @@ set -eo pipefail
 cd CI-CDApp
 echo "building and testing"
 echo "$PWD"
-sleep 40s
-xcodebuild -project CI-CDApp.xcodeproj -scheme CI-CDApp -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 7,OS=latest" CODE_SIGNING_REQUIRED=NO COSECODE_SIGNING_ALLOWED="NO" clean build test| xcpretty
+sleep 30s
+rm -rf DerivedData
+xcodebuild -project CI-CDApp.xcodeproj -scheme CI-CDApp -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 7,OS=latest" CODE_SIGNING_REQUIRED=NO COSECODE_SIGNING_ALLOWED="NO" -derivedDataPath DerivedData clean test build| xcpretty
