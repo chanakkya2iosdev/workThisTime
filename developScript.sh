4bcd568a5b4b4907882cff830e6431221fd6ab89
hillromChanakkya <chanakya.mathi@hill-rom.com>
2019-05-15 21:23:06 -0400

    updated script

--- developScript.sh
+++ developScript.sh
@@ -9,6 +9,6 @@ set -eo pipefail
 cd CI-CDApp
 echo "building and testing"
 echo "$PWD"
-sleep 30s
+sleep 40s
 rm -rf DerivedData
 xcodebuild -project CI-CDApp.xcodeproj -scheme CI-CDApp -sdk iphonesimulator -destination "platform=iOS Simulator,name=iPhone 7,OS=latest" CODE_SIGNING_REQUIRED=NO COSECODE_SIGNING_ALLOWED="NO" -parallel-testing-enabled YES -parallel-testing-worker-count 4 -derivedDataPath DerivedData clean test build | xcpretty
